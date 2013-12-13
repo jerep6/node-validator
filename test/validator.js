1046cb77e8bbd26e39359157e93a57e91000b89f
gregbacchus <greg+git@bacchus.co.nz>
2013-12-13 21:25:55 +1300

    added tests for validator.run

--- test/validator.js
+++ test/validator.js
@@ -32,60 +32,58 @@ function expectSuccess(err, details) {
   err.should.be.eql(0);
 }
 
-describe('custom Xor validator', function () {
-  it('should require "a" to be a string',
-    function (done) {
-
-      validator.run(testArray, [
-        { 'a': [ { z: 1 } ] }
-      ], expectFailure);
-
+describe( 'validator.run', function(){
+  it('should call the callback on pass',
+    function (done){
+      var count = 0;
+      function callback(errorCount)
+      {
+        count++;
+        errorCount.should.be.equal(0);
+      }
+      validator.run({validate: function(){}}, null, callback);
+      count.should.be.equal(1);
       done();
     });
 
-  it('should require "b" to be a string',
-    function (done) {
-
-      validator.run(testArray, [
-        { 'b': 17 }
-      ], expectFailure);
-
+  it('should call the callback on failure',
+    function (done){
+      var count = 0;
+      function callback(errorCount)
+      {
+        count++;
+        errorCount.should.be.equal(1);
+      }
+      validator.run({validate: function(value, onError){ onError(); }}, null, callback);
+      count.should.be.equal(1);
       done();
     });
 
-  it('should not allow both "a" and "b" to be present',
-    function (done) {
-      validator.run(testArray, [
-        { 'a': "hello", 'b': 'world' }
-      ], expectFailure);
+  it('should return multiple errors',
+    function (done){
+      var count = 0;
+      function callback(errorCount, errorList)
+      {
+        count++;
+        errorCount.should.be.equal(2);
+        errorList.should.be.have.length(2);
 
-      done();
-    });
+        errorList[0].should.be.type('object');
+        errorList[0].should.have.property('message', 'error1');
+        errorList[0].should.have.property('parameter', 'a');
+        errorList[0].should.have.property('value', 'test');
 
-  it('should not allow unexpected fields',
-    function (done) {
-      validator.run(testArray, [
-        { 'a': "hello", 'c': '!' }
-      ], expectFailure);
-
-      done();
-    });
-
-  it('should be valid as there is only "a"',
-    function (done) {
-      validator.run(testArray, [
-        { 'a': "hello" }
-      ], expectSuccess);
-
-      done();
-    });
-
-  it('should be valid as there is only "b"',
-    function (done) {
-      validator.run(testArray, [
-        { 'b': "world" }
-      ], expectSuccess);
-
+        errorList[1].should.be.type('object');
+        errorList[1].should.have.property('message', 'error2');
+        errorList[1].should.have.property('parameter', 'b');
+        errorList[1].should.have.property('value', 123);
+      }
+      function validate(value, onError){
+        onError('error1', 'a', 'test');
+        onError('error2', 'b', 123);
+      }
+      validator.run({validate: validate}, null, callback);
+      count.should.be.equal(1);
       done();
     });
 });
@@ -208,3 +206,62 @@ describe('validator', function () {
       done();
     });
 });
+
+describe('custom Xor validator', function () {
+  it('should require "a" to be a string',
+    function (done) {
+
+      validator.run(testArray, [
+        { 'a': [ { z: 1 } ] }
+      ], expectFailure);
+
+      done();
+    });
+
+  it('should require "b" to be a string',
+    function (done) {
+
+      validator.run(testArray, [
+        { 'b': 17 }
+      ], expectFailure);
+
+      done();
+    });
+
+  it('should not allow both "a" and "b" to be present',
+    function (done) {
+      validator.run(testArray, [
+        { 'a': "hello", 'b': 'world' }
+      ], expectFailure);
+
+      done();
+    });
+
+  it('should not allow unexpected fields',
+    function (done) {
+      validator.run(testArray, [
+        { 'a': "hello", 'c': '!' }
+      ], expectFailure);
+
+      done();
+    });
+
+  it('should be valid as there is only "a"',
+    function (done) {
+      validator.run(testArray, [
+        { 'a': "hello" }
+      ], expectSuccess);
+
+      done();
+    });
+
+  it('should be valid as there is only "b"',
+    function (done) {
+      validator.run(testArray, [
+        { 'b': "world" }
+      ], expectSuccess);
+
+      done();
+    });
+});
+
