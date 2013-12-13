0dc51aa5bc26775055f450ce5787a405c1e8d7bc
gregbacchus <greg+git@bacchus.co.nz>
2013-12-13 21:46:33 +1300

    renamed run test file

--- test/run.js
+++ test/run.js
@@ -2,38 +2,40 @@ var should = require('should');
 
 var validator = require('../lib/validator');
 
-describe( 'validator.run', function(){
+describe('validator.run', function () {
   it('should call the callback on pass',
-    function (done){
+    function (done) {
       var count = 0;
-      function callback(errorCount)
-      {
+
+      function callback(errorCount) {
         count++;
         errorCount.should.be.equal(0);
       }
-      validator.run({validate: function(){}}, null, callback);
+
+      validator.run({validate: function () {}}, null, callback);
       count.should.be.equal(1);
       done();
     });
 
   it('should call the callback on failure',
-    function (done){
+    function (done) {
       var count = 0;
-      function callback(errorCount)
-      {
+
+      function callback(errorCount) {
         count++;
         errorCount.should.be.equal(1);
       }
-      validator.run({validate: function(value, onError){ onError(); }}, null, callback);
+
+      validator.run({validate: function (value, onError) { onError(); }}, null, callback);
       count.should.be.equal(1);
       done();
     });
 
   it('should return multiple errors',
-    function (done){
+    function (done) {
       var count = 0;
-      function callback(errorCount, errorList)
-      {
+
+      function callback(errorCount, errorList) {
         count++;
         errorCount.should.be.equal(2);
         errorList.should.be.have.length(2);
@@ -48,10 +50,12 @@ describe( 'validator.run', function(){
         errorList[1].should.have.property('parameter', 'b');
         errorList[1].should.have.property('value', 123);
       }
-      function validate(value, onError){
+
+      function validate(value, onError) {
         onError('error1', 'a', 'test');
         onError('error2', 'b', 123);
       }
+
       validator.run({validate: validate}, null, callback);
       count.should.be.equal(1);
       done();
